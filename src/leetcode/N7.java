7f8a13d61c63437a764a096c810bd8d861496890
Jay <beginin15@gmail.com>
2020-10-21 22:24:57 +0900

    [#3] LeetCode - 7. Reverse Integer (math)

---
+++ src/leetcode/N7.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class N7 {
+    public int reverse(int x) {
+        long numberX = (x >= 0) ? x : ((long)(x) * -1);
+        StringBuilder conversion = new StringBuilder(String.valueOf(numberX));
+        conversion.reverse();
+        long value = Long.parseLong(conversion.toString());
+        if(Integer.MIN_VALUE < value && value < Integer.MAX_VALUE) {
+            int intValue = (int) value;
+            return x > 0 ? intValue : intValue * -1;
+        }
+        else
+            return 0;
+    }
+}
