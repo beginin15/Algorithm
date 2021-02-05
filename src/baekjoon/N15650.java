797538d02c06d4f2b31d829b1b8c7f8ef47f5eb3
Jay <beginin15@gmail.com>
2021-02-05 21:02:58 +0900

    [#51] 백준 - 15650. N과 M(2) (재귀, 백트래킹)
    다른 풀이

--- src/baekjoon/N15650.java
+++ src/baekjoon/N15650.java
@@ -5,11 +5,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class N15650 {
@@ -18,12 +13,10 @@ public class N15650 {
     private static int m;
     private static int[] arr;
     private static boolean[] isUsed;
-    private static Set<String> seq = new LinkedHashSet<>();
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
@@ -31,32 +24,27 @@ public class N15650 {
         arr = new int[m];
         isUsed = new boolean[10];
 
-        func(0);
+        func(0, 0);
 
-        for (String s : seq) {
-            bw.write(s);
-        }
         bw.close();
         br.close();
     }
 
-    private static void func(int k) {
+    private static void func(int k, int pre) throws IOException {
         if (k == m) {
-            int[] copy = Arrays.copyOfRange(arr, 0, m);
-            Arrays.sort(copy);
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < m; i++) {
-                sb.append(copy[i]).append(' ');
+                sb.append(arr[i]).append(' ');
             }
             sb.append("\n");
-            seq.add(sb.toString());
+            bw.write(sb.toString());
             return;
         }
-        for (int i = 1; i <= n; i++) {
+        for (int i = pre + 1; i <= n; i++) {
             if (!isUsed[i]) {
                 arr[k] = i;
                 isUsed[i] = true;
-                func(k + 1);
+                func(k + 1, i);
                 isUsed[i] = false;
             }
         }
