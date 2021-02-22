538b9074ec6f6af9a106791c44572ba725aadb28
Jay <beginin15@gmail.com>
2021-02-23 02:35:27 +0900

    [#66] failed: 백준 - 1799. 비숍 (재귀, 백트래킹)
    - for문으로 순회 시도

---
+++ src/baekjoon/N1799_try_2.java
@@ -0,0 +1,91 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class N1799_try_2 {
+    private static int size;
+    private static boolean[] canPlaced;
+    private static boolean[] output;
+    private static boolean[] isUsedLT;
+    private static boolean[] isUsedLB;
+    private static int max;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        size = Integer.parseInt(br.readLine());
+        canPlaced = new boolean[size * size];
+        output = new boolean[size * size];
+        isUsedLT = new boolean[size * 2 - 1];
+        isUsedLB = new boolean[size * 2 - 1];
+
+        // 1. 초기화
+        for (int i = 0; i < size; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < size; j++) {
+                canPlaced[i * size + j] = Integer.parseInt(st.nextToken()) == 1;
+            }
+        }
+
+        // 2. 백트래킹
+        func(0);
+
+        // 3. 결과 출력
+        bw.write(max + "\n");
+        bw.close();
+        br.close();
+    }
+
+    private static void func(int idx) {
+        if (idx == size * size) {
+            count();
+//            print(output);
+            return;
+        }
+        for (int i = 0; i < size * size; i++) {
+            if (!canPlaced[i]) {
+                continue;
+            }
+            int row = i / size;
+            int col = i % size;
+            int lt = row + col;
+            int lb = row - col + size - 1;
+            if (isUsedLT[lt] || isUsedLB[lb]) {
+                continue;
+            }
+            output[i] = isUsedLT[lt] = isUsedLB[lb] = true;
+            func(idx + 1);
+            isUsedLT[lt] = isUsedLB[lb] = false;
+        }
+    }
+
+    private static void count() {
+        int count = 0;
+        for (int i = 0; i < size * size; i++) {
+            if (output[i]) {
+                count++;
+            }
+        }
+        if (count > max) {
+            max = count;
+        }
+        print(output);
+    }
+
+    private static void print(boolean[] output) {
+        System.out.println("=====================");
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.printf("%2s", output[i * size + j] ? "O" : "X");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
